1ea4b76fc5b5f86d2069a1a52cf1e601cfd440b6
derick321 <derick321@kaist.ac.kr>
2024-12-05 04:10:00 +0900

    Hide delete button if not administrator

--- frontend/app/projects/project-item.tsx
+++ frontend/app/projects/project-item.tsx
@@ -13,6 +13,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy, faTrash, faUsers } from "@fortawesome/free-solid-svg-icons";
 import Project, { ProjectT } from "@/src/API/project";
 import { useRouter } from "next/navigation";
+import { UserContext } from "@/utils/user-context";
 
 interface ProjectItemProps {
   project: Project;
@@ -25,6 +26,8 @@ const ProjectItem = ({ project, deleteProject }: ProjectItemProps) => {
     onOpenChange: onProjectSettingOpenChange,
   } = useDisclosure();
   const { id, title, administrator, members, videos } = project.data;
+  const { user } = React.useContext(UserContext);
+  const isAdmin = Number(administrator.id) === user?.data.id;
   const router = useRouter();
 
   const copyInvitationCode = React.useCallback(() => {
@@ -45,22 +48,28 @@ const ProjectItem = ({ project, deleteProject }: ProjectItemProps) => {
         <h1 className="font-bold text-xl text-foreground">{title}</h1>
       </CardBody>
       <CardFooter className="!p-0 flex flex-row justify-end py-2 items-center gap-x-4">
-        <Tooltip content="Delete Project" placement="top">
+        {isAdmin ? (
+          <Tooltip content="Delete Project" placement="top">
+            <div>
+              <Button
+                isIconOnly
+                className="bg-background"
+                radius="full"
+                size="sm"
+                onPress={() => deleteProject(project)}
+              >
+                <FontAwesomeIcon
+                  className="text-default-600 size-3"
+                  icon={faTrash}
+                />
+              </Button>
+            </div>
+          </Tooltip>
+        ) : (
           <div>
-            <Button
-              isIconOnly
-              className="bg-background"
-              radius="full"
-              size="sm"
-              onPress={() => deleteProject(project)}
-            >
-              <FontAwesomeIcon
-                className="text-default-600 size-3"
-                icon={faTrash}
-              />
-            </Button>
+            <Button className="bg-transparent" size="sm"></Button>
           </div>
-        </Tooltip>
+        )}
       </CardFooter>
     </Card>
   );

8c87037d00225df0307c2b413f083203f0acf6f9
Nuzup Shadiev <nuzupshadiev@gmail.com>
2024-12-10 21:04:52 +0900

    a

--- frontend/app/video/[id]/comments.tsx
+++ frontend/app/video/[id]/comments.tsx
@@ -140,7 +140,7 @@ export default function CommentsSection({
             the first to comment!
           </p>
         ) : (
-          <div className="overflow-y-scroll w-full max-h-[500px]">
+          <div className="overflow-y-scroll w-full max-h-[540px]">
             {sortedInTimeTexts.map((comment) => (
               <Comment
                 key={comment.id}
